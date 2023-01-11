185da0418e5228d8aee88c7eb79ffcd439eb14d3
CJ Kucera <cj@apocalyptech.com>
2023-01-11 09:30:39 -0600

    Moving Fast Travel / Teleport animation tweak up near the top of the mod

--- Apocalyptech/qol/mega_timesaver_xl/gen_mega_timesaver_xl.py
+++ Apocalyptech/qol/mega_timesaver_xl/gen_mega_timesaver_xl.py
@@ -231,6 +231,54 @@ for obj_name in [
             obj_data['LinearAcceleration']*2)
 mod.newline()
 
+# Make Fast Travel + Teleport digistruct animations disappear
+# (note that the death respawn is totally separate from this, and handled via
+# some AnimSequence tweaks above)
+mod.header('Fast Travel / Teleport Animation Disable')
+
+# A bunch of silliness in here, when I was looking at bundling this separately
+# as its own mod and was considering multiple versions.  In the end, this just
+# hardcodes an effective disabling of the whole sequence, and I could omit
+# various bits of math.  But whatever, the work is done -- leaving it as-is.
+default_duration = 6.5
+default_unlock = 5.5
+default_teleport = 1.5
+unlock_scale = default_unlock/default_duration
+teleport_scale = default_teleport/default_duration
+
+# 0.5 - Effectively disables it
+# 2 - Quite short, but still gets a tiny bit of the tunnel
+new_duration = 0.5
+
+min_timers = min(default_teleport, new_duration)
+
+mod.reg_hotfix(Mod.PATCH, '',
+        '/Game/PlayerCharacters/_Shared/_Design/Travel/Action_TeleportEffects.Default__Action_TeleportEffects_C',
+        'Duration',
+        new_duration,
+        )
+
+# Adjust delay on unlocking resources (whatever that means; haven't figured out
+# what's not available when "locked")
+mod.bytecode_hotfix(Mod.PATCH, '',
+        '/Game/PlayerCharacters/_Shared/_Design/Travel/Action_TeleportEffects',
+        'ExecuteUbergraph_Action_TeleportEffects',
+        1119,
+        default_unlock,
+        max(min_timers, round(new_duration*unlock_scale, 6)),
+        )
+
+# Adjust delay on actually teleporting
+mod.bytecode_hotfix(Mod.PATCH, '',
+        '/Game/PlayerCharacters/_Shared/_Design/Travel/Action_TeleportEffects',
+        'ExecuteUbergraph_Action_TeleportEffects',
+        391,
+        default_teleport,
+        max(min_timers, round(new_duration*teleport_scale, 6)),
+        )
+
+mod.newline()
+
 # Vault Card Chest injection!  So: the Vault Card menu uses a custom red chest
 # to do the animation, and the chest object in the UI structure also uses a custom
 # opening animation.  Those objects don't ordinarily exist until the Vault Card
@@ -1879,54 +1927,6 @@ mod.bytecode_hotfix(Mod.LEVEL, 'Camp_P',
         34634,
         5,
         5/global_scale)
-mod.newline()
-
-# Make Fast Travel + Teleport digistruct animations disappear
-# (note that the death respawn is totally separate from this, and handled via
-# some AnimSequence tweaks above)
-mod.header('Fast Travel / Teleport Animation Disable')
-
-# A bunch of silliness in here, when I was looking at bundling this separately
-# as its own mod and was considering multiple versions.  In the end, this just
-# hardcodes an effective disabling of the whole sequence, and I could omit
-# various bits of math.  But whatever, the work is done -- leaving it as-is.
-default_duration = 6.5
-default_unlock = 5.5
-default_teleport = 1.5
-unlock_scale = default_unlock/default_duration
-teleport_scale = default_teleport/default_duration
-
-# 0.5 - Effectively disables it
-# 2 - Quite short, but still gets a tiny bit of the tunnel
-new_duration = 0.5
-
-min_timers = min(default_teleport, new_duration)
-
-mod.reg_hotfix(Mod.PATCH, '',
-        '/Game/PlayerCharacters/_Shared/_Design/Travel/Action_TeleportEffects.Default__Action_TeleportEffects_C',
-        'Duration',
-        new_duration,
-        )
-
-# Adjust delay on unlocking resources (whatever that means; haven't figured out
-# what's not available when "locked")
-mod.bytecode_hotfix(Mod.PATCH, '',
-        '/Game/PlayerCharacters/_Shared/_Design/Travel/Action_TeleportEffects',
-        'ExecuteUbergraph_Action_TeleportEffects',
-        1119,
-        default_unlock,
-        max(min_timers, round(new_duration*unlock_scale, 6)),
-        )
-
-# Adjust delay on actually teleporting
-mod.bytecode_hotfix(Mod.PATCH, '',
-        '/Game/PlayerCharacters/_Shared/_Design/Travel/Action_TeleportEffects',
-        'ExecuteUbergraph_Action_TeleportEffects',
-        391,
-        default_teleport,
-        max(min_timers, round(new_duration*teleport_scale, 6)),
-        )
-
 mod.newline()
 
 # Turns out the Dandelion slots are identical, bytecodewise (at least for these two offsets)
